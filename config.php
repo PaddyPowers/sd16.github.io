1205a672cdf68782c8d713d23affa87baa96cbe1
danielBKRonan <d.b.k.ronan@dundee.ac.uk>
2015-10-14 08:25:02 +0100

    upload config online

--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 ini_set( "display_errors", true );
 date_default_timezone_set( "Australia/Sydney" );  // http://www.php.net/manual/en/timezones.php
 define( "DB_DSN", "mysql:host=localhost;dbname=sd16" );
-define( "DB_USERNAME", "sd16" );
-define( "DB_PASSWORD", "xBQucvDL29FhgY" );
+define( "DB_USERNAME", "username" );
+define( "DB_PASSWORD", "password" );
 define( "CLASS_PATH", "classes" );
 define( "TEMPLATE_PATH", "templates" );
 define( "HOMEPAGE_NUM_ARTICLES", 999 );
-define( "ADMIN_USERNAME", "sd16student" );
-define( "ADMIN_PASSWORD", "BxksmZbP4HU5ChS7" );
+define( "ADMIN_USERNAME", "username" );
+define( "ADMIN_PASSWORD", "password" );
 define( "ARTICLE_IMAGE_PATH", "images/articles" );
 define( "IMG_TYPE_FULLSIZE", "fullsize" );
 define( "IMG_TYPE_THUMB", "thumb" );
