16cfa269677cc0356c1e65c219053c327c1e77d7
Petr Chutný <petr.chutny@gmail.com>
2015-11-30 17:02:11 +0000

    Setting the password to the previous, human-rememberable one

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ define( "CLASS_PATH", "classes" );
 define( "TEMPLATE_PATH", "templates" );
 define( "HOMEPAGE_NUM_ARTICLES", 999 );
 define( "ADMIN_USERNAME", "admin" );
-define( "ADMIN_PASSWORD", "ff43c90f87d90859c8f618fe6961538b" );
+define( "ADMIN_PASSWORD", "f1d506565336aa20bda1d3b0e292abd2" );
 define( "ARTICLE_IMAGE_PATH", "images/articles" );
 define( "IMG_TYPE_FULLSIZE", "fullsize" );
 define( "IMG_TYPE_THUMB", "thumb" );
